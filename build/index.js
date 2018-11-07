1be7b0aeebf9d8455a382d281cf3d682a853b6e0
Paulius Juknis <paulius.juknis@gmail.com>
2018-11-07 13:55:22 +0200

    setup eslint with rules

--- build/index.js
+++ build/index.js
@@ -1,13 +1,30 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-console.log('hello');
-
+// Testing ES6 transpilation to ES5
 var test = 'Hello';
+// debugger;
+console.log(test);
 
-var StartPCAcademy = function StartPCAcademy() {
-  _classCallCheck(this, StartPCAcademy);
+var StartPCAcademy = function () {
+  function StartPCAcademy() {
+    _classCallCheck(this, StartPCAcademy);
 
-  console.log('StartPCAcademy class created');
-};
+    console.log('StartPCAcademy class created');
+  }
+
+  _createClass(StartPCAcademy, [{
+    key: 'start',
+    value: function start() {
+      console.log('PC Academy started!');
+    }
+  }]);
+
+  return StartPCAcademy;
+}();
+
+var startPCAcademy = new StartPCAcademy();
+startPCAcademy.start();
--- task1_text_area/index.js
+++ task1_text_area/index.js
@@ -1,9 +1,17 @@
-console.log('hello');
-
+// Testing ES6 transpilation to ES5
 let test = 'Hello';
+// debugger;
+console.log(test);
 
 class StartPCAcademy {
   constructor() {
     console.log('StartPCAcademy class created');
   }
+
+  start() {
+    console.log('PC Academy started!');
+  }
 }
+
+const startPCAcademy = new StartPCAcademy();
+startPCAcademy.start();
