b7ea731e15c2741adf11969c078c901e42a83aa7
Paulius Juknis <paulius.juknis@gmail.com>
2018-11-07 09:23:56 +0200

    fixed babel build now transpiles es6

---
+++ build/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+console.log('hello');
+
+var test = 'Hello';
+
+var StartPCAcademy = function StartPCAcademy() {
+  _classCallCheck(this, StartPCAcademy);
+
+  console.log('StartPCAcademy class created');
+};
--- task1_text_area/index.js
+++ task1_text_area/index.js
@@ -1,5 +1,7 @@
 console.log('hello');
 
+let test = 'Hello';
+
 class StartPCAcademy {
   constructor() {
     console.log('StartPCAcademy class created');
