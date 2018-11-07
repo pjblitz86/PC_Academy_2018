44a9fb8470f962759dc3c077ffc108dafe34d123
Paulius Juknis <paulius.juknis@gmail.com>
2018-11-07 16:25:24 +0200

    start implement selectors and logic

--- task1_text_area/index.js
+++ task1_text_area/index.js
@@ -1,8 +1,29 @@
-const textArea = document.querySelector("#textArea");
+const textArea = document.querySelector('#textArea');
 console.log(textArea);
+let rowAttributeValue = textArea.getAttribute('rows');
+let colsAttributeValue = textArea.getAttribute('cols');
+let currentTextValue;
+let currentTextLength;
 
-// textArea.addEventListener('onchange', handleOnChange);
+console.log(`row count: ${rowAttributeValue}`);
+console.log(`row width: ${colsAttributeValue} chars`);
 
-// const handleOnChange = (e) => {
-//   console.log(e);
-// };
+textArea.addEventListener('input', handleTextChange);
+
+function handleTextChange(e) {
+  currentTextValue = e.target.value;
+  currentTextLength = currentTextValue.length;
+  console.log(currentTextValue);
+  console.log(currentTextLength);
+  if (currentTextLength <= colsAttributeValue) {
+    handleRowDecrease();
+  }
+  if (currentTextLength > colsAttributeValue) {
+    rowAttributeValue += 1;
+    currentTextLength = '0';
+  }
+}
+function handleRowDecrease() {
+
+  rowAttributeValue = textArea.setAttribute('rows', 1);
+}
