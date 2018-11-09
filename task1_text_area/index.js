a84e9bfdb32a0fa66d94c2122f4adfd57ee38603
Paulius Juknis <paulius.juknis@gmail.com>
2018-11-09 13:36:47 +0200

    fixed minRows=2 reseting to 1 problem

--- task1_text_area/index.js
+++ task1_text_area/index.js
@@ -1,7 +1,11 @@
+// problems
+// scrollbar appearing lowers cols size
+// flickering cuz of arrays - use scroll example
+
 const textArea = document.querySelector('#textArea');
 let rows = parseInt(textArea.getAttribute('rows'));
 let cols = parseInt(textArea.getAttribute('cols'));
-// let minRows = 1;
+let minRows = 2;
 let maxRows = 4;
 let inputValue;
 let inputArr;
@@ -18,12 +22,16 @@ class TextArea {
     inputValue = e.target.value;
     inputArr = inputValue.split('\n');
     rows = inputArr.length;
+    // neveikia for
     for (let i = 0; i < inputArr.length; i++) {
       rows += parseInt(inputArr[i].length / cols);
     }
     if (rows > maxRows) {
       rows = maxRows;
       textArea.classList.add('add-scrollbar');
+    } else if (rows < minRows) {
+      rows = minRows;
+      // textArea.setAttribute('rows', rows);
     } else {
       textArea.setAttribute('rows', rows);
       textArea.classList.remove('add-scrollbar');
