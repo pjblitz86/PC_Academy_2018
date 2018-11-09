2affa1f8aabf9543c5f9b9930d2975fd60bc1f11
Paulius Juknis <paulius.juknis@gmail.com>
2018-11-09 10:02:32 +0200

    textArea resize implemented in es6 class need more tests

--- task1_text_area/index.js
+++ task1_text_area/index.js
@@ -1,56 +1,37 @@
-// selectors
 const textArea = document.querySelector('#textArea');
-let rowCount = Number(textArea.getAttribute('rows'));
-let colsCount = textArea.getAttribute('cols');
+let rows = parseInt(textArea.getAttribute('rows'));
+let cols = parseInt(textArea.getAttribute('cols'));
 // let minRows = 1;
 let maxRows = 4;
 let inputValue;
-let inputLength;
+let inputArr;
 
-// event listeners
-textArea.addEventListener('keyup', onInput);
-  
-function onInput(e) {
+class TextArea {
 
-  textArea.classList.remove('no-resize');
-  textArea.classList.add('allow-resize');
-  textArea.classList.add('remove-scrollbar');
-  textArea.classList.remove('add-scrollbar');
-  
-  inputValue = e.target.value;
-  inputLength = inputValue.length;
-  textArea.classList.remove('scroll-bar');
-  // logic
-  // handleRowDecrease();
+  constructor() {
 
-  if (inputLength % colsCount === 0 || e.keyCode === 13) {
-    handleRowIncrease();
   }
-  
-  textArea.classList.remove('allow-resize');
-  textArea.classList.add('no-resize');
+
+  resize(e) {
+    textArea.classList.remove('no-resize');
+    textArea.classList.add('allow-resize');
+    inputValue = e.target.value;
+    inputArr = inputValue.split('\n');
+    rows = inputArr.length;
+    for (let i = 0; i < inputArr.length; i++) {
+      rows += parseInt(inputArr[i].length / cols);
+    }
+    if (rows > maxRows) {
+      rows = maxRows;
+      textArea.classList.add('add-scrollbar');
+    } else {
+      textArea.setAttribute('rows', rows);
+      textArea.classList.remove('add-scrollbar');
+    }
+    textArea.classList.remove('allow-resize');
+    textArea.classList.add('no-resize');
+  }
 }
 
-// function handleRowDecrease() {
-//   if ((rowCount < maxRows) && inputLength % colsCount === 0) {
-//     textArea.classList.remove('add-scrollbar');
-//   } else {
-//     rowCount -= 1;
-//     textArea.setAttribute('rows', rowCount);
-//   }
-// }
-
-function handleRowIncrease() {
-  if (rowCount > maxRows) {
-    textArea.classList.add('add-scrollbar');
-  } else {
-    rowCount += 1;
-    textArea.setAttribute('rows', rowCount);
-  }
-  
-  console.log('********************');
-  console.log('row count', rowCount);
-  console.log('text value:', inputValue);
-  console.log('text length:', inputLength);
-  
-}
+const tA = new TextArea();
+textArea.addEventListener('keyup', tA.resize);
