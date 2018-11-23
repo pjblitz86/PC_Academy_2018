bac4f7d54a2978da65295707b2ab34671fc5eafc
Paulius Juknis <paulius.juknis@gmail.com>
2018-11-23 10:40:45 +0200

    added npm start script to monitor.js

--- task3_node_cpu_ram/cpu.js
+++ task3_node_cpu_ram/cpu.js
@@ -3,7 +3,7 @@ var os = require('os-utils');
 function calculateCpuUsage() {
   os.cpuUsage((v) => {
     const cpuUsed = (v * 100).toFixed(2);
-    console.log(`CPU used (%): ${cpuUsed}`); // problem first result is not converted to % Delay async?
+    console.log(`CPU used (%): ${cpuUsed}`);
   });
 }
 
