23a36f93aed42ed3b02948f7aa3e5514a5cb3c91
Paulius Juknis <paulius.juknis@gmail.com>
2018-11-27 11:05:17 +0200

    connected to mongoDB

---
+++ DevConnector_Project/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://pjblitz86:admin123@ds261253.mlab.com:61253/pj86devconnector'
+};
--- DevConnector_Project/server.js
+++ DevConnector_Project/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const port = require('./config');
+const port = require('./config/port');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello'));
+const db = require('./config/keys').mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('mongoDB connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello!!!'));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
