5f9e011bdce06c0fb7f621aeef36d49ae73a0a6d
Paulius Juknis <paulius.juknis@gmail.com>
2018-11-27 11:22:34 +0200

    add endpoints

--- DevConnector_Project/server.js
+++ DevConnector_Project/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const port = require('./config/port');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 const db = require('./config/keys').mongoURI;
@@ -11,5 +15,9 @@ mongoose
   .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello!!!'));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
