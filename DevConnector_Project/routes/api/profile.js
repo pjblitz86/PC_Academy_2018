6c1d5deffb571bd15a2a6db1ed00eb8ec6f8638f
Paulius Juknis <paulius.juknis@gmail.com>
2018-11-30 14:04:40 +0200

    more profile api routes

--- DevConnector_Project/routes/api/profile.js
+++ DevConnector_Project/routes/api/profile.js
@@ -6,9 +6,9 @@ const validateProfileInput = require('../../validation/profile');
 
 router.get('/test', (req, res) => res.json({ msg: "profile works" }));
 
+
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
   const errors = {};
-
   Profile.findOne({ user: req.user.id })
     .populate('user', ['name', 'avatar'])
     .then(profile => {
@@ -21,13 +21,53 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
     .catch(err => res.status(404).json(err));
 });
 
+router.get('/all', (req, res) => {
+  const errors = {};
+  Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofile = 'There are no profiles';
+        return res.status(404).json(errors);
+      }
+      res.json(profiles);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+  Profile.findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+  Profile.findOne({ user: req.params.user_id })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
   const { errors, isValid } = validateProfileInput(req.body);
-
   if (!isValid) {
     return res.status(400).json(errors);
   }
-
   const profileFields = {};
   profileFields.user = req.user.id;
   if (req.body.handle) profileFields.handle = req.body.handle;
@@ -41,7 +81,6 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
   if (typeof req.body.skills !== 'undefined') {
     profileFields.skills = req.body.skills.split(',');
   }
-
   profileFields.social = {};
   if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
   if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
