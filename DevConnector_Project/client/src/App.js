6aaa278b9bd121fa2005d53533ac4e51aa7f35f3
Paulius Juknis <paulius.juknis@gmail.com>
2018-12-17 09:49:53 +0200

    implement logout functionality

--- DevConnector_Project/client/src/App.js
+++ DevConnector_Project/client/src/App.js
@@ -9,13 +9,20 @@ import { Provider } from 'react-redux';
 import store from './store';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 import './App.css';
 
 if (localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
   const decoded = jwt_decode(localStorage.jwtToken);
   store.dispatch(setCurrentUser(decoded));
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+
+    window.location.href = '/login';
+  }
+
 }
 
 class App extends Component {
