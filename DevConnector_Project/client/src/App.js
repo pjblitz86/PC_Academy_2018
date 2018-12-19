5bcda18dfd6db456b9d577d49321feced65026ae
Paulius Juknis <paulius.juknis@gmail.com>
2018-12-19 09:43:44 +0200

    profile Reducer and getCurrentProfile

--- DevConnector_Project/client/src/App.js
+++ DevConnector_Project/client/src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
@@ -11,6 +12,7 @@ import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
 import './App.css';
+import { clearCurrentProfile } from './actions/profileActions';
 
 if (localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
@@ -19,7 +21,7 @@ if (localStorage.jwtToken) {
   const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
     store.dispatch(logoutUser());
-
+    store.dispatch(clearCurrentProfile());
     window.location.href = '/login';
   }
 
@@ -36,6 +38,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
