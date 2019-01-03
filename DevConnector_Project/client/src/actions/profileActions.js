6d4fb6be6a0a501abdd374753e6f885a552dccdc
Paulius Juknis <paulius.juknis@gmail.com>
2019-01-03 10:27:30 +0200

    profileActions component and delete profile functionality

--- DevConnector_Project/client/src/actions/profileActions.js
+++ DevConnector_Project/client/src/actions/profileActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS } from './types';
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from './types';
 
 export const getCurrentProfile = () => dispatch => {
   dispatch(setProfileLoading());
@@ -22,6 +22,23 @@ export const createProfile = (profileData, history) => dispatch => {
       type: GET_ERRORS,
       payload: err.response.data
     }));
+}
+
+export const deleteAccount = () => dispatch => {
+  if (window.confirm('Are you sure? This can not be undone!')) {
+    axios.delete('/profile')
+      .then(res => {
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      })
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        }))
+  }
 }
 
 export const setProfileLoading = () => {
