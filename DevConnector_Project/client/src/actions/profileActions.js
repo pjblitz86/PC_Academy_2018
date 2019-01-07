6986438d1dffb7473dee832c206a5a5c90a936c2
Paulius Juknis <paulius.juknis@gmail.com>
2019-01-07 13:21:14 +0200

    Dashboard Experience display and delete

--- DevConnector_Project/client/src/actions/profileActions.js
+++ DevConnector_Project/client/src/actions/profileActions.js
@@ -57,8 +57,21 @@ export const addEducation = (eduData, history) => dispatch => {
       type: GET_ERRORS,
       payload: err.response.data
     }))
-
 }
+
+export const deleteExperience = (id) => dispatch => {
+  axios
+    .delete(`/profile/experience/${id}`)
+    .then(res => dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    }))
+    .catch(err => dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    }))
+}
+
 
 export const deleteAccount = () => dispatch => {
   if (window.confirm('Are you sure? This can not be undone!')) {
