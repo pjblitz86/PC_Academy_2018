bb0eaa7b199c4c8057077160ff023e161be52548
Paulius Juknis <paulius.juknis@gmail.com>
2019-01-10 11:51:37 +0200

    fix landing page not-found component rendering

--- DevConnector_Project/client/src/App.js
+++ DevConnector_Project/client/src/App.js
@@ -44,22 +44,22 @@ class App extends Component {
             <Navbar />
             <Switch>
               <Route exact path="/" component={Landing} />
+              <div className="container">
+                <Switch>
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/profiles" component={Profiles} />
+                  <Route exact path="/profile/:handle" component={Profile} />
+                  <Route exact path="/not-found" component={NotFound} />
+                  <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                  <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+                  <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+                  <PrivateRoute exact path="/add-experience" component={AddExperience} />
+                  <PrivateRoute exact path="/add-education" component={AddEducation} />
+                  <Route path="*" component={NotFound} />
+                </Switch>
+              </div>
             </Switch>
-            <div className="container">
-              <Switch>
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/profiles" component={Profiles} />
-                <Route exact path="/profile/:handle" component={Profile} />
-                <Route exact path="/not-found" component={NotFound} />
-                <PrivateRoute exact path="/dashboard" component={Dashboard} />
-                <PrivateRoute exact path="/create-profile" component={CreateProfile} />
-                <PrivateRoute exact path="/edit-profile" component={EditProfile} />
-                <PrivateRoute exact path="/add-experience" component={AddExperience} />
-                <PrivateRoute exact path="/add-education" component={AddEducation} />
-                <Route exact component={NotFound} />
-              </Switch>
-            </div>
             <Footer />
           </div>
         </Router>
--- DevConnector_Project/client/src/components/layout/Landing.js
+++ DevConnector_Project/client/src/components/layout/Landing.js
@@ -19,7 +19,7 @@ class Landing extends Component {
             <div className="row">
               <div className="col-md-12 text-center">
                 <h1 className="display-3 mb-4">Developer Connector</h1>
-                <p className="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
+                <p className="lead"> Create a developer profile, share posts and get help from other developers</p>
                 <hr />
                 <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
                 <Link to="/login" className="btn btn-lg btn-light">Login</Link>
