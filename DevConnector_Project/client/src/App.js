274714813cb0365852b039610bca88d5f0f17d3d
Paulius Juknis <paulius.juknis@gmail.com>
2019-01-11 09:26:14 +0200

    add Posts and PostForm component

--- DevConnector_Project/client/src/App.js
+++ DevConnector_Project/client/src/App.js
@@ -20,6 +20,7 @@ import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import NotFound from './components/not-found/NotFound';
 
 if (localStorage.jwtToken) {
@@ -55,6 +56,7 @@ class App extends Component {
                     <PrivateRoute exact path="/edit-profile" component={EditProfile} />
                     <PrivateRoute exact path="/add-experience" component={AddExperience} />
                     <PrivateRoute exact path="/add-education" component={AddEducation} />
+                    <PrivateRoute exact path="/feed" component={Posts} />
                     <Route path="*" component={NotFound} />
                   </Switch>
                 </div>
--- DevConnector_Project/client/src/reducers/postReducer.js
+++ DevConnector_Project/client/src/reducers/postReducer.js
@@ -1,3 +1,5 @@
+import { ADD_POST } from '../actions/types';
+
 const initialState = {
   posts: [],
   post: {},
@@ -7,6 +9,11 @@ const initialState = {
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [action.payload, ...state.posts]
+      }
     default:
       return state;
   }
