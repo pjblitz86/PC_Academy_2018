a1d716793a17ef5071eab2d3dbb9d09d48b96677
Paulius Juknis <paulius.juknis@gmail.com>
2019-01-16 12:42:14 +0200

    path fix

--- DevConnector_Project/server.js
+++ DevConnector_Project/server.js
@@ -35,7 +35,7 @@ app.use('/posts', posts);
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('./client/build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
