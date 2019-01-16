64006e6659bb34ddafee4bb6c4f0b6c8f63c1e6e
Paulius Juknis <paulius.juknis@gmail.com>
2019-01-16 11:11:22 +0200

    fix paths

--- DevConnector_Project/server.js
+++ DevConnector_Project/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
-
+const port = process.env.PORT || 5000;
 const profile = require('./routes/profile');
 const posts = require('./routes/posts');
 const users = require('./routes/users');
@@ -35,13 +35,12 @@ app.use('/posts', posts);
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('./client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './client', 'build', 'index.html'));
   });
 }
 
-const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 module.exports = app;
