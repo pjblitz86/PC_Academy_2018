5e4f978671f3762a0123e9beea6b96efa7e565dc
Paulius Juknis <paulius.juknis@gmail.com>
2019-01-22 14:49:06 +0200

    fix api url

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -17,9 +17,8 @@ class ProfileGithub extends Component {
     const { username } = this.props;
     const { count, sort, clientId, clientSecret } = this.state;
 
-    fetch(`https:/api.github.com/users/${username}/repos?per_page=${count}&sort=${sort}&client_id=${clientId}&client_secret=${clientSecret}`)
-      .then(res => res.text())
-      .then(text => JSON.parse(text))
+    fetch(`https://api.github.com/users/${username}/repos?per_page=${count}&sort=${sort}&client_id=${clientId}&client_secret=${clientSecret}`)
+      .then(res => res.json())
       .then(data => {
         if (this.refs.myRef) {
           this.setState({ repos: data })
